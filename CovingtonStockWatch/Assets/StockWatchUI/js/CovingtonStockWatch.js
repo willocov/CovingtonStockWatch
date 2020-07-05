ac429bfe3cf8610eecb48b9e2e321bd744bb08ac
William Covington <willocov@gmail.com>
2020-07-05 04:07:21 -0700

    Added JS functions to navigate screens. Added bootstrap icons to project

--- CovingtonStockWatch/Assets/StockWatchUI/js/CovingtonStockWatch.js
+++ CovingtonStockWatch/Assets/StockWatchUI/js/CovingtonStockWatch.js
@@ -37,3 +37,103 @@ function gotoNotificationViewerScreen(){
     $("#NotificationManagerScreen").css("visibility", "hidden");
     $("#NotificationManagerScreen").hide();
 }
+
+function gotoStockManagerScreen(){
+    console.log("Stock Manager Screen");
+
+    //Hide Stock Viewer Screen
+    $("#StockViewerScreen").css("visibility", "hidden");
+    $("#StockViewerScreen").hide();
+
+    //Show Notification Viewer Screen
+    $("#NotificationViewerScreen").css("visibility", "hidden");
+    $("#NotificationViewerScreen").hide();
+
+    //Hide Stock Manager Screen
+    $("#StockManagererScreen").css("visibility", "visible");
+    $("#StockManagererScreen").show();
+
+    //Hide Notification Manager Screen
+    $("#NotificationManagerScreen").css("visibility", "hidden");
+    $("#NotificationManagerScreen").hide();
+}
+
+function gotoNotificationManagerScreen(){
+    console.log("Notification Manager Screen");
+
+    //Hide Stock Viewer Screen
+    $("#StockViewerScreen").css("visibility", "hidden");
+    $("#StockViewerScreen").hide();
+
+    //Show Notification Viewer Screen
+    $("#NotificationViewerScreen").css("visibility", "hidden");
+    $("#NotificationViewerScreen").hide();
+
+    //Hide Stock Manager Screen
+    $("#StockManagererScreen").css("visibility", "hidden");
+    $("#StockManagererScreen").hide();
+
+    //Hide Notification Manager Screen
+    $("#NotificationManagerScreen").css("visibility", "visible");
+    $("#NotificationManagerScreen").show();
+}
+
+function HandleSwipe(direction){
+    //This function handles screen navigation with swiping behavior
+    //Allows user to swipe between viewer screens
+    //Swiping right on a manager screen will return to the respective viewer screen
+
+    if($("#StockViewerScreen").is(":visible")){
+        if(direction == "left"){
+            gotoNotificationViewerScreen();
+        }
+    }
+    else if($("#NotificationViewerScreen").is(":visible")){
+        if(direction == "right"){
+            gotoStockViewerScreen();
+        }
+    }
+    else if($("#StockManagerScreen").is(":visible")){
+        if(direction == "right"){
+            gotoStockViewerScreen();
+        }
+    }
+    else if($("#NotificationManagerScreen").is(":visible")){
+        gotoNotificationViewerScreen();
+    }
+}
+
+//Profile Related Functions
+function LoadProfiles(){
+    
+}
+
+function SaveProfiles(){
+
+}
+
+function AddProfile(){
+
+}
+
+function RemoveProfile(){
+
+}
+
+//Stock Related Functions
+function AddStock(){}
+
+function RemoveStock(){}
+
+function ClearAllStocks(){}
+
+function LoadStocks(){}
+
+//Notification Related Functions
+function AddNotification(){}
+
+function RemoveNotification(){}
+
+function ClearAllNotifications(){}
+
+function LoadNotifications(){}
